c215c0b6923ad3033733a073eafdfb0c0330d166
nataliia <nromanen.softserveinc@gmail.com>
2024-12-09 18:02:38 +0100

    add main method

--- src/main/java/model/Group.java
+++ src/main/java/model/Group.java
@@ -1,2 +1,13 @@
-package model;public class Group {
+package model;
+
+public class Group {
+    private String name;
+
+    public Group(String name){
+        this.name = name;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
 }
