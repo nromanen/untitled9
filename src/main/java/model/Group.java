a3e49ffc1c7067eeaa2e541dfd4766af984305c7
nromanen <nromanen.softserveinc@gmail.com>
2024-12-09 18:41:32 +0100

    Update Group.java

--- src/main/java/model/Group.java
+++ src/main/java/model/Group.java
@@ -3,9 +3,6 @@ package model;
 public class Group {
     private String name;
 
-    public Group(String name){
-        this.name = name;
-    }
 
     public static void main(String[] args) {
         System.out.println("Hello");
